8bc8a9148391c235f9c609c6033b8f0e730a8c8e
Sahil Ahmed <sa.idesofmarch@gmail.com>
2024-08-30 18:57:32 +0530

    added icon and sound to public and remove commented code

--- src/general.ts
+++ src/general.ts
@@ -34,24 +34,6 @@ export const getNotificationPermission = async () => {
 async function checkNotificationPermissionGranted(
 	permission: string,
 ) {
-	// console.log(
-	// 	"apiKey:",
-	// 	import.meta.env.VITE_FIREBASE_API_KEY,
-	// 	"authDomain:",
-	// 	import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	// 	"databaseURL:",
-	// 	import.meta.env.VITE_FIREBASE_DATABASE_URL,
-	// 	"projectId:",
-	// 	import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	// 	"storageBucket:",
-	// 	import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	// 	"messagingSenderId:",
-	// 	import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	// 	"appId:",
-	// 	import.meta.env.VITE_FIREBASE_APP_ID,
-	// 	"measurementId:",
-	// 	import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-	// );
 	if (permission === "granted") {
 		console.log("Notification permission has been granted.");
 
